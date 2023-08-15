db14e0019e3e3b2ea5811511cc23ab3ec6338e7a
Rahulk850 <rahulkumar8505067715@gmail.com>
2023-08-15 18:04:36 +0530

    db updated

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook"
+const mongoURI = `mongodb+srv://rahulkumar8505067715:Rahul2001@cluster0.qwhdvk4.mongodb.net/?retryWrites=true&w=majority`
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo Successfully");
